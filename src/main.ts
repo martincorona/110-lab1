91b1725cc3180d440970ea62c1798ec47f5b31be
martincorona <m4corona@ucsd.edu>
2025-10-02 15:02:40 -0700

    lemon

--- src/main.ts
+++ src/main.ts
@@ -12,17 +12,39 @@ const layer = new Konva.Layer();
 stage.add(layer);
 
 // create our shape
-const circle = new Konva.Circle({
+
+const lemonBody = new Konva.Ellipse({
   x: stage.width() / 2,
   y: stage.height() / 2,
-  radius: 70,
-  fill: 'red',
+  radiusX: 100, // horizontal radius (makes it wider)
+  radiusY: 65,  // vertical radius (makes it shorter)
+  fill: 'yellow',
+  stroke: 'black',
+  strokeWidth: 4
+});
+
+const leftBump = new Konva.Circle({
+  x: stage.width() / 2 - 100, // position it at the left edge of the ellipse
+  y: stage.height() / 2,
+  radius: 15,
+  fill: 'yellow',
+  stroke: 'black',
+  strokeWidth: 4
+});
+
+const rightBump = new Konva.Circle({
+  x: stage.width() / 2 + 100, // position it at the right edge of the ellipse
+  y: stage.height() / 2,
+  radius: 15,
+  fill: 'yellow',
   stroke: 'black',
   strokeWidth: 4
 });
 
 // add the shape to the layer
-layer.add(circle);
+layer.add(lemonBody);
+layer.add(leftBump);
+layer.add(rightBump);
 
 // add the layer to the stage
 stage.add(layer);
